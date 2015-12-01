b21752de3d8861606219479068af5dff6cccea99
Lourenzoni <leandro.lourenzoni@gmail.com>
2015-12-01 21:53:22 -0200

    Fazendo testes funcionar com chai

--- test/test.js
+++ test/test.js
@@ -1,35 +1,30 @@
-var assert = require('assert');
 var expect = require('chai').expect;
-var POINTS = 100000;
 
 describe('CanVis', function() {
 	describe('Performance', function () {
 		describe('Criação das séries', function () {
-			it('Array sem tamanho e push', function () {
+			it('Deve criar série com 400 mil pontos em menos de 100 ms', function () {
+				var serie = [], step = (Math.PI/180);
 				timer.start();
-				arraySizelessAndPush();
-				delta = timer.finish();
-				assert.equal(delta, 100, 'Rodou em menos de 100ms');
+				for (var i=0; i < 400000; i++) {
+					serie.push( step * i );
+				}
+				timer.finish();
+				expect(timer.elapsed()).to.be.below(100);
+			});
+			it('Deve criar série com 400 mil pontos em menos de 10 ms', function () {
+				var serie = new Array(400000);
+				var step = Math.PI/180;
+				timer.start();
+				for (var i=0; i < 400000; i++) {
+					serie[i] = step * i;
+				}
+				timer.finish();
+				expect(timer.elapsed()).to.be.below(10);
 			});
 		});
 	});
 });
-
-function arraySizelessAndPush() {
-	var serie = [];
-	var step = (Math.PI/180);
-	for (var i=0; i < POINTS; i++) {
-		serie.push( step * i );
-	}
-}
-
-function arraySizelessAndPush() {
-	var serie = [];
-	var step = (Math.PI/180);
-	for (var i=0; i < POINTS; i++) {
-		serie.push( step * i );
-	}
-}
 
 var timer = {
 	start: function() {
@@ -38,11 +33,13 @@ var timer = {
 	finish: function() {
 		if (this.counter) {
 			var diffArray = process.hrtime(this.counter);
-			var diff = (diffArray[0] * 1e3 + diffArray[1] * 1e-6).toFixed(2);
-			console.log('Time: %d ms', diff);
+			this.diff = (diffArray[0] * 1e3 + diffArray[1] * 1e-6).toFixed(3);
 			this.counter = 0;
-			return diff;
+			//console.log('Time: %d ms', this.diff);
 		}
-	}
+	},
+	elapsed: function() {
+		return this.diff;
+	},
 }
 
